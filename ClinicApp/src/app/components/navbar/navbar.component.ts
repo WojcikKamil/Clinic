e82c62ceed993aa617d001c0475eb5797c21fbba
Kamil Wójcik <60044272+WojcikKamil@users.noreply.github.com>
2021-10-11 14:57:34 +0200

    Navbar-added-buttons-logout-feature

--- ClinicApp/src/app/components/navbar/navbar.component.ts
+++ ClinicApp/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import AccountService from 'src/app/services/account.service';
+import { MyPofileDialogComponent } from '../dialog/my-pofile-dialog/my-pofile-dialog.component';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +10,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+  public isMenuCollapsed = true;
+  constructor(public accountService:AccountService,
+    private router : Router,
+    private dialog: MatDialog) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+  }
+
+  logout(){
+    this.accountService.logout();
+    this.router.navigate(['/home']);
+  }
+
+  openMyProfileDialog(): void{
+    this.dialog.open(MyPofileDialogComponent)
   }
 
 }
