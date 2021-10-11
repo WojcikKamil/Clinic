451a66678f23f796d8c104d3cd499062584dbf70
Kamil Wójcik <60044272+WojcikKamil@users.noreply.github.com>
2021-10-11 15:14:05 +0200

    Changed-page-size-lenght

--- ClinicAPI/ClinicAPI/Models/RequestParams.cs
+++ ClinicAPI/ClinicAPI/Models/RequestParams.cs
@@ -7,9 +7,9 @@ namespace ClinicAPI.Models
 {
     public class RequestParams
     {
-        const int maxPageSize = 10;
+        const int maxPageSize = 50;
         public int PageNumber { get; set; } = 1;
-        private int _pageSize = 2;
+        private int _pageSize = 20;
 
         public int PageSize
         {
